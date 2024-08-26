8db46f25292f1b5ac5b62edee1d31adecd8d0f8a
baqnngg <il8696546@gmail.com>
2024-08-26 20:14:30 +0900

    portfolio

--- pages/index.js
+++ pages/index.js
@@ -13,7 +13,7 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-
+      
       <h1>병준이의 포트폴리오</h1>
     </Layout>
   );
