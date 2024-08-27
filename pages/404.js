98a21d1878d935484cb39dacfada3a5ec73bef28
baqnngg <il8696546@gmail.com>
2024-08-27 20:10:22 +0900

    portfolio

--- pages/404.js
+++ pages/404.js
@@ -1,9 +1,10 @@
+import Layout from "@/components/layout";
 
 export default function PageNotFound(){
     return(
-        <>
-        <h1>페이지를 찾을 수 없습니다.</h1>
-        </>
+        <Layout>
+            <h1>페이지를 찾을 수 없습니다.</h1>
+        </Layout>
     )
 }
 
--- pages/about-me.js
+++ pages/about-me.js
@@ -1,9 +1,10 @@
+import Layout from "@/components/layout";
 
 export default function AboutMe(){
     return(
-        <div>
+        <Layout>
             <h1>자기소개가 들어갈 공간</h1>
-        </div>
+        </Layout>
     )
 }
 
